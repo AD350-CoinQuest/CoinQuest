1d87b6cc8324f3fbc82268e280a3eb2926922ff9
Bade Habib <42555252+JungDefiant@users.noreply.github.com>
2026-05-10 10:31:17 -0700

    feat: Added Expo client and NestJS server files (#1)
    * feat: Added Expo client and NestJS server files
    
    * fix: Fixed issue with server folder not being committed, compiled gitignore files into one file
    
    * Add CoinQuest ERD and documentation
    
    * feat: added components from Figma
    
    * Setup Expo and connect to Supabase
    
    ---------
    
    Co-authored-by: BHabibMeetKai <bade.habib@meetkai.com>
    Co-authored-by: Mozog90 <mozog90@gmail.com>

---
+++ client/src/components/button.tsx
@@ -0,0 +1,50 @@
+import { StyleSheet } from "react-native-unistyles";
+import { View, Text } from "react-native";
+
+import type { ViewStyle, StyleProp } from "react-native";
+
+export interface ButtonProps {
+	/** Used to override the default root style. */
+	style?: StyleProp<ViewStyle>;
+	/** Used to locate this view in end-to-end tests. */
+	testID?: string;
+}
+
+export function Button(props: ButtonProps) {
+	return (
+		<View testID={props.testID ?? "72:123"} style={[styles.root, props.style]}>
+			<View testID="72:122" style={styles.background} />
+			<Text testID="72:124" style={styles.buttonLabel}>
+				{`Button Label`}
+			</Text>
+		</View>
+	);
+}
+
+const styles = StyleSheet.create((theme) => ({
+	root: {
+		width: 480,
+		height: 79,
+	},
+	background: {
+		width: 480,
+		height: 79,
+		borderBottomLeftRadius: 8,
+		borderBottomRightRadius: 8,
+		borderTopLeftRadius: 8,
+		borderTopRightRadius: 8,
+		borderWidth: 3,
+		borderStyle: "solid",
+		borderColor: "rgba(1, 1, 1, 1)",
+		backgroundColor: "rgba(253, 253, 253, 1)",
+	},
+	buttonLabel: {
+		color: "rgba(1, 1, 1, 1)",
+		textAlign: "center",
+		fontFamily: "Inter",
+		fontSize: 24,
+		fontStyle: "normal",
+		fontWeight: "300",
+		letterSpacing: 2,
+	},
+}));
